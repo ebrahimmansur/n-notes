b66c18ec90194b233cad8363741c5e689f291e90
Ebrahim Mansur <ebrahimmansur90117@gmail.com>
2023-02-24 16:53:44 +0200

    Add:  project

---
+++ N.Notes/N.Notes.Api/HostRunner.cs
@@ -0,0 +1,35 @@
+﻿namespace N.Notes.Api
+{
+    public class HostRunner
+    {
+        public static void Run(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+          
+
+            builder.Services.AddControllers();
+          
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ N.Notes/N.Notes.Api/Program.cs
@@ -0,0 +1,8 @@
+namespace N.Notes.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args) => HostRunner.Run(args);
+       
+    }
+}
